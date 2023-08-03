1ffb6430c550b3b6de30a27d12ca432c355d199f
MasterXiong <harryfootball@163.com>
2023-08-03 18:30:39 +0100

    add derl

---
+++ derl/envs/__init__.py
@@ -0,0 +1,8 @@
+# don't need this as we have registered when importing metamorph code
+# from gym.envs.registration import register
+
+# register(
+#     id="Unimal-v0",
+#     entry_point="derl.envs.tasks.task:make_env",
+#     max_episode_steps=1000,
+# )
