81edf031aedd4a4aae2456573fe34854bc3cb838
MasterXiong <harryfootball@163.com>
2024-01-23 15:09:18 +0000

    improve dagger

--- metamorph/algos/distill/buffer.py
+++ metamorph/algos/distill/buffer.py
@@ -9,16 +9,17 @@ from metamorph.config import cfg
 
 class Buffer(object):
     def __init__(self, obs_space, act_shape):
-        T, P = cfg.DAGGER.ITER_STEPS, cfg.PPO.NUM_ENVS
+        N, T, P = cfg.DAGGER.ITERS, cfg.DAGGER.ITER_STEPS, cfg.PPO.NUM_ENVS
+        self.P = P
 
         if isinstance(obs_space, gym.spaces.Dict):
             self.obs = {}
             for obs_type, obs_space_ in obs_space.spaces.items():
-                self.obs[obs_type] = torch.zeros(T, P, *obs_space_.shape)
+                self.obs[obs_type] = torch.zeros(N*T, P, *obs_space_.shape)
         else:
-            self.obs = torch.zeros(T, P, *obs_space.shape)
+            self.obs = torch.zeros(N*T, P, *obs_space.shape)
 
-        self.act_mean = torch.zeros(T, P, *act_shape)
+        self.act_target = torch.zeros(N*T, P, *act_shape)
 
         self.step = 0
 
@@ -28,51 +29,38 @@ class Buffer(object):
                 self.obs[obs_type] = self.obs[obs_type].to(device)
         else:
             self.obs = self.obs.to(device)
-        self.act_mean = self.act_mean.to(device)
+        self.act_target = self.act_target.to(device)
 
-    def insert(self, obs, act_mean):
+    def insert(self, obs, act_target):
         if isinstance(obs, dict):
             for obs_type, obs_val in obs.items():
-                self.obs[obs_type][self.step] = obs_val
+                self.obs[obs_type][self.step] = obs_val.cpu()
         else:
-            self.obs[self.step] = obs
-        self.act_mean[self.step] = act_mean
+            self.obs[self.step] = obs.cpu()
+        self.act_target[self.step] = act_target.cpu()
 
-        self.step = (self.step + 1) % DAGGER.ITER_STEPS
+        # self.step = (self.step + 1) % cfg.DAGGER.ITER_STEPS
+        self.step += 1
 
-    def get_sampler(self, adv):
-        dset_size = cfg.PPO.TIMESTEPS * adv.shape[1]
+    def get_sampler(self):
 
-        assert dset_size >= cfg.PPO.BATCH_SIZE
+        dset_size = self.step * self.P
+        assert dset_size >= cfg.DAGGER.BATCH_SIZE
 
         sampler = BatchSampler(
             SubsetRandomSampler(range(dset_size)),
-            cfg.PPO.BATCH_SIZE,
+            cfg.DAGGER.BATCH_SIZE,
             drop_last=True,
         )
 
         for idxs in sampler:
             batch = {}
-            batch["ret"] = self.ret[:, self.idx].view(-1, 1)[idxs]
-
             if isinstance(self.obs, dict):
                 batch["obs"] = {}
                 for ot, ov in self.obs.items():
-                        batch["obs"][ot] = ov[:, self.idx].view(-1, *ov.size()[2:])[idxs]
+                        batch["obs"][ot] = ov[:self.step].view(-1, *ov.size()[2:])[idxs].cuda()
             else:
-                batch["obs"] = self.obs[:, self.idx].view(-1, *self.obs.size()[2:])[idxs]
+                batch["obs"] = self.obs[:self.step].view(-1, *self.obs.size()[2:])[idxs].cuda()
 
-            batch["val"] = self.val[:, self.idx].view(-1, 1)[idxs]
-            batch["act"] = self.act[:, self.idx].view(-1, self.act.size(-1))[idxs]
-            batch["adv"] = adv.view(-1, 1)[idxs]
-            batch["logp_old"] = self.logp[:, self.idx].view(-1, 1)[idxs]
-            # batch["dropout_mask_v"] = self.dropout_mask_v.view(-1, 12, 128)[idxs]
-            # batch["dropout_mask_mu"] = self.dropout_mask_mu.view(-1, 12, 128)[idxs]
-            batch["unimal_ids"] = self.unimal_ids[:, self.idx].view(-1)[idxs]
-            # batch["limb_logp_old"] = self.limb_logp.view(-1, 24)[idxs]
+            batch["act_target"] = self.act_target[:self.step].view(-1, self.act_target.size(-1))[idxs].cuda()
             yield batch
-
-    def filter(self, process_with_error):
-        self.idx = [True for _ in range(cfg.PPO.NUM_ENVS)]
-        for p in process_with_error:
-            self.idx[p] = False
--- metamorph/algos/distill/dagger.py
+++ metamorph/algos/distill/dagger.py
@@ -6,6 +6,7 @@ import yaml
 
 import numpy as np
 import torch
+import torch.nn as nn
 import torch.optim as optim
 
 from metamorph.config import cfg, get_default_cfg
@@ -14,6 +15,9 @@ from metamorph.algos.ppo.inherit_weight import restore_from_checkpoint
 from metamorph.algos.ppo.envs import *
 from metamorph.algos.distill.buffer import Buffer
 from metamorph.utils import file as fu
+
+torch.manual_seed(0)
+np.random.seed(0)
 
 
 def convert_dict_to_list(config_dict, prefix):
@@ -37,26 +41,38 @@ class DAggerTrainer:
         with open(f'{cfg.DAGGER.TEACHER_PATH}/config.yaml', 'r') as f:
             teacher_config = yaml.safe_load(f)
         teacher_model_config = convert_dict_to_list(teacher_config['MODEL'], 'MODEL')
-        cfg.merge_from_list(teacher_model_config)
+        for i in range(len(teacher_model_config) // 2):
+            try:
+                cfg.merge_from_list(teacher_model_config[(i*2):(i+1)*2])
+            except:
+                continue
         # initialize the envs with the teacher's observation types
         # hardcode here, as in the code we can only rebuild the student's obs with the teacher's obs, not vice versa
         self.envs = make_vec_envs(training=False)
         # load the teacher model
         self.teacher = ActorCritic(self.envs.observation_space, self.envs.action_space).cuda()
         # use the teacher's obs normalizer
-        self.obs_rms, _, _ = restore_from_checkpoint(self.teacher, cp_path=f'{cfg.DAGGER.TEACHER_PATH}/Unimal-v0.pt')
-        set_ob_rms(self.envs, self.obs_rms)
+        obs_rms, _, _ = restore_from_checkpoint(self.teacher, cp_path=f'{cfg.DAGGER.TEACHER_PATH}/Unimal-v0.pt')
+        set_ob_rms(self.envs, obs_rms)
 
         with open(f'{cfg.DAGGER.STUDENT_PATH}/config.yaml', 'r') as f:
             student_config = yaml.safe_load(f)
         student_model_config = convert_dict_to_list(student_config['MODEL'], 'MODEL')
-        cfg.merge_from_list(student_model_config)
+        for i in range(len(student_model_config) // 2):
+            try:
+                cfg.merge_from_list(student_model_config[(i*2):(i+1)*2])
+            except:
+                continue
         # student envs is only used to set the state and action space of the student model
         student_envs = make_vec_envs(training=False)
         # load the model
         self.student = ActorCritic(student_envs.observation_space, student_envs.action_space).cuda()
-        restore_from_checkpoint(self.student, cp_path=f'{cfg.DAGGER.STUDENT_PATH}/checkpoint_{cfg.DAGGER.STUDENT_CHECKPOINT}.pt')
+        # the student and teacher share the same obs_rms based on our distillation code
+        self.student_obs_rms, _, _ = restore_from_checkpoint(self.student, cp_path=f'{cfg.DAGGER.STUDENT_PATH}/checkpoint_{cfg.DAGGER.STUDENT_CHECKPOINT}.pt')
+        # buffer
+        self.buffer = Buffer(student_envs.observation_space, student_envs.action_space.shape)
 
+        # TODO: student may have different context features
         if os.path.exists(f'{cfg.ENV.WALKER_DIR}/context_v2.pkl'):
             with open(f'{cfg.ENV.WALKER_DIR}/context_v2.pkl', 'rb') as f:
                 self.student_context = pickle.load(f)
@@ -72,7 +88,6 @@ class DAggerTrainer:
             with open(f'{cfg.ENV.WALKER_DIR}/context_v2.pkl', 'wb') as f:
                 pickle.dump(self.student_context, f)
 
-        # self.student_buffer = Buffer(self.envs.observation_space, self.envs.action_space.shape).cuda()
         self.optimizer = optim.Adam(self.student.parameters(), lr=cfg.DAGGER.BASE_LR, eps=cfg.DAGGER.EPS, weight_decay=cfg.DAGGER.WEIGHT_DECAY)
 
     def convert_obs(self, teacher_obs, unimal_ids):
@@ -94,34 +109,66 @@ class DAggerTrainer:
 
         obs = self.envs.reset()
         for cur_iter in range(cfg.DAGGER.ITERS):
-            loss = 0.
             for step in range(cfg.DAGGER.ITER_STEPS):
                 unimal_ids = self.envs.get_unimal_idx()
                 # teacher
                 with torch.no_grad():
                     _, teacher_pi, _, _ = self.teacher(obs, compute_val=False)
                 teacher_act_mean = teacher_pi.loc
+                teacher_act_sample = teacher_pi.sample()
                 # student
                 student_obs = self.convert_obs(obs, unimal_ids)
-                _, student_pi, _, _ = self.student(student_obs, compute_val=False)
+                with torch.no_grad():
+                    _, student_pi, _, _ = self.student(student_obs, compute_val=False)
                 student_act_mean = student_pi.loc
+                student_act_sample = student_pi.sample()
                 # Sample actions
-                act = student_pi.sample()
-                next_obs, reward, done, infos = self.envs.step(act)
-                # compute action loss
-                loss += (((student_act_mean - teacher_act_mean).square() * (1 - obs['act_padding_mask'].float())).sum(dim=-1) / (1 - obs['act_padding_mask'].float()).sum(dim=1)).mean()
-                
-                obs = next_obs
-
-            loss /= cfg.DAGGER.ITER_STEPS
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
+                anneal_threshold = 1. - cur_iter / cfg.DAGGER.ITERS
+                if cur_iter < 10:
+                    anneal_threshold = 1. - cur_iter / 10
+                    choose_teacher = (torch.rand(teacher_act_mean.shape[0], 1) <= anneal_threshold).float().cuda()
+                    act = choose_teacher * teacher_act_sample + (1. - choose_teacher) * student_act_sample
+                else:
+                    act = student_act_sample
+                # save to buffer
+                self.buffer.insert(student_obs, teacher_act_mean)
+                # env step
+                obs, reward, done, infos = self.envs.step(act)
+            
+            print (f'Train for iter {cur_iter}')
+            self.train_on_buffer()
 
             self.save_sampled_agent_seq(cur_iter)
             if (cur_iter + 1) % cfg.DAGGER.SAVE_FREQ == 0:
-                torch.save([self.student.state_dict(), self.obs_rms], f'{cfg.OUT_DIR}/checkpoint_{cur_iter + 1}.pt')
+                torch.save([self.student.state_dict(), self.student_obs_rms], f'{cfg.OUT_DIR}/checkpoint_{cur_iter + 1}.pt')
 
+    def train_on_buffer(self):
+
+        update_num = 0
+        early_stop = False
+        batch_losses = []
+        for i in range(cfg.DAGGER.EPOCH_PER_ITER):
+            batch_sampler = self.buffer.get_sampler()
+            for j, batch in enumerate(batch_sampler):
+                _, student_pi, _, _ = self.student(batch["obs"], compute_val=False)
+                student_act_mean = student_pi.loc
+                # compute action loss
+                loss = (((student_act_mean - batch['act_target']).square() * (1 - batch["obs"]['act_padding_mask'].float())).sum(dim=-1) / (1 - batch["obs"]['act_padding_mask'].float()).sum(dim=1)).mean()
+                batch_losses.append(loss.item())
+
+                self.optimizer.zero_grad()
+                loss.backward()
+                norm = nn.utils.clip_grad_norm_(self.student.parameters(), 0.5)
+                self.optimizer.step()
+                update_num += 1
+                if update_num % 10 == 0:
+                    print (f'avg loss over last 10 batches: {np.mean(batch_losses[-10:])}')
+                if update_num == cfg.DAGGER.MINIBATCH_UPDATE_PER_ITER:
+                    early_stop = True
+                    break
+
+            if early_stop:
+                break
 
     def save_sampled_agent_seq(self, cur_iter):
         
